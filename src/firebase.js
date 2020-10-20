933f344463111396e6a392c98979bc13ca114462
nelleeeee <rudghksldl@gmail.com>
2020-10-20 23:53:48 +0900

    init

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxsHVlUrRNBjQNvB1Ple97HIPOsufRWF8",
+  authDomain: "telegram-clone-d42cf.firebaseapp.com",
+  databaseURL: "https://telegram-clone-d42cf.firebaseio.com",
+  projectId: "telegram-clone-d42cf",
+  storageBucket: "telegram-clone-d42cf.appspot.com",
+  messagingSenderId: "260496525010",
+  appId: "1:260496525010:web:ecb670a78440a3793b8f0b",
+  measurementId: "G-5XL21EHGYB",
+};
